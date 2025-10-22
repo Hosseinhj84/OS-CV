7a10a0b00e6e3dbf4d1a9a269afa1476945ed599
Hosseinhj84 <125790298+Hosseinhj84@users.noreply.github.com>
2025-10-22 05:12:00 +0000

    تنظیم مجدد مسیرهای فایل‌های استاتیک در تنظیمات

--- OS_CV/OS_CV/settings.py
+++ OS_CV/OS_CV/settings.py
@@ -119,12 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# مسیر فایل‌های static پروژه (در زمان توسعه)
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-# مسیر نهایی جمع‌آوری فایل‌ها (برای collectstatic)
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
